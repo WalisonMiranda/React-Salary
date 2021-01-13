9eeffdce077765b10b9bd83ced65b7b1bf65d114
Walison Miranda <walisonshido@gmail.com>
2021-01-13 17:43:13 -0300

    Updates

---
+++ precache-manifest.35a91b4f42452c0dae268ffc1a85266f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c2b4d228104e3d4988591c95960b34",
+    "url": "/React-Salary/index.html"
+  },
+  {
+    "revision": "e19c94808b24301efd41",
+    "url": "/React-Salary/static/css/2.4da19627.chunk.css"
+  },
+  {
+    "revision": "1361abe3de5fa1d6dac5",
+    "url": "/React-Salary/static/css/main.464df1a8.chunk.css"
+  },
+  {
+    "revision": "e19c94808b24301efd41",
+    "url": "/React-Salary/static/js/2.aa700c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Salary/static/js/2.aa700c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1361abe3de5fa1d6dac5",
+    "url": "/React-Salary/static/js/main.379b3fae.chunk.js"
+  },
+  {
+    "revision": "8f8a8657c47ac7117768",
+    "url": "/React-Salary/static/js/runtime-main.5ad3e92d.js"
+  }
+]);
